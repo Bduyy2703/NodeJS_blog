3a421d4edc14124f057c4c136b19a644c888e22a
Bduyy2703 <nguyenduy7003@gmail.com>
2024-09-24 10:59:41 +0700

    Refactor code: creat route , app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars'); // Import đúng cách express-handlebars
 const app = express();
 const port = 3000;
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname , 'public')))
-
+app.use(express.urlencoded(
+  {
+    extended : true
+  }
+))
+app.use(express.json())
 // Ghi log http
 app.use(morgan('combined'));
 
@@ -21,13 +27,10 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Xử lý route
-app.get('/', (req, res) => {
-  res.render('home');
-});
 
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+route(app);
+
+
 // Khởi động server
 app.listen(port, () => {
   console.log(`Ứng dụng đang lắng nghe tại cổng ${port}`);
